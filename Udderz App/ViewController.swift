bb35b83360378454c6a569ab4338207fc80fd810
Gilbert Gonzalez <gg633@scarletmail.rutgers.edu>
2021-02-04 00:04:33 -0500

    Initial Commit

---
+++ Udderz App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udderz App
+//
+//  Created by Gilbert Gonzalez on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
